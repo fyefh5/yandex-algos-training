107bdf8999756ae83fba55393042895f03a8db5d
Anuar <118943209+fyefh5@users.noreply.github.com>
2024-09-05 13:26:08 +0300

    hw1d: amend from NO SOLUTIONS to NO SOLUTION

--- hw1/hw1d.py
+++ hw1/hw1d.py
@@ -3,14 +3,14 @@ b = int(input())
 c = int(input())
 
 if c < 0:
-    print("NO SOLUTIONS")
+    print("NO SOLUTION")
 elif a == 0:
     if b == c**2:
         print("MANY SOLUTIONS")
     else:
-        print("NO SOLUTIONS")
+        print("NO SOLUTION")
 else:
     if (c**2 - b)%a == 0:
         print((c**2 - b)//a)
     else:
-        print("NO SOLUTIONS")
+        print("NO SOLUTION")
