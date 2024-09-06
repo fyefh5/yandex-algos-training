c5a921a8bb5fc82e125ff29def9cb23fe66a95e6
Anuar <118943209+fyefh5@users.noreply.github.com>
2024-09-06 21:58:13 +0300

    hw1g: k//m>0 condition checked

--- hw1/hw1g.py
+++ hw1/hw1g.py
@@ -1,11 +1,12 @@
 def details(n, k, m):
     count = 0
-    while n // k > 0:
-        remainder1 = n % k
-        workpieces = n // k
-        remainder2 = (k % m) * workpieces
-        count += k // m * workpieces
-        n = remainder1 + remainder2
+    if k // m > 0:
+        while n // k > 0:
+            remainder1 = n % k
+            workpieces = n // k
+            remainder2 = (k % m) * workpieces
+            count += k // m * workpieces
+            n = remainder1 + remainder2
     return count
 
 # assert details(10, 5, 2) == 4
@@ -16,6 +17,13 @@ def main():
     n, k, m = map(int, input().split())
     ans = details(n,k,m)
     print(ans)
+
+def check():
+    for n in range(1, 201):
+        for k in range(1, 201):
+            for m in range(1, 201):
+                print(n,k,m,details(n,k,m))
+                
 
 if __name__ == "__main__":
     main()
