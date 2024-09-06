230ff1c0d804f3e452efbda46a69de2005e3f14f
Anuar <118943209+fyefh5@users.noreply.github.com>
2024-09-06 21:12:11 +0300

    hw1f: from internet: tests with assertions and __name__ execution

--- hw1/hw1f.py
+++ hw1/hw1f.py
@@ -1,26 +1,23 @@
-a1, a2, b1, b2 = list(map(int, input().split()))
+def laptops(a1, b1, a2, b2):
+    combinations = [
+        (a1 + a2, max(b1, b2)),
+        (a1 + b2, max(b1, a2)),
+        (max(a1, a2), b1 + b2),
+        (max(a1, b2), b1 + a2)
+    ]
 
-areas = [0]*4
-# hstack
-areas[0] = max(a1, b1) * (a2 + b2)
-areas[1] = max(a1, b2) * (a2 + b1)
-# vstack
-areas[2] = (a1 + b1) * max(a2, b2)
-areas[3] = (a1 + b2) * max(a2, b1)
+    return min(combinations, key=lambda y: y[0] * y[1])
 
-minarea = areas[0]
-iminarea = 0
-for i, area in enumerate(areas):
-    if area < minarea:
-        minarea = area
-        iminarea = i
 
-match iminarea:
-    case 0:
-        print(f"{max(a1, b1)} {a2 + b2}")
-    case 1:
-        print(f"{max(a1, b2)} {a2 + b1}")
-    case 2:
-        print(f"{max(a2, b2)} {a1 + b1}")
-    case 3:
-        print(f"{max(a2, b1)} {a1 + b2}")
+assert laptops(10, 2, 2, 10) == (20, 2)
+assert laptops(5, 7, 3, 2) == (5, 9)
+
+
+def main():
+    a1, b1, a2, b2 = map(int, input().split())
+    result = laptops(a1, b1, a2, b2)
+    print(f"{result[0]} {result[1]}")
+
+
+if __name__ == '__main__':
+    main()
