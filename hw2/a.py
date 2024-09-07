aa845795d29d9b9964406d2e1d3527ce17aaf185
Anuar <118943209+fyefh5@users.noreply.github.com>
2024-09-07 20:27:21 +0300

    hw2a: solved open tests, but wrong answer

---
+++ hw2/a.py
@@ -0,0 +1,18 @@
+def monotonical(arr):
+    if len(arr) == 0 or len(arr) == 1:
+        return "NO"
+    for i in range(1,len(arr)):
+        if arr[i-1] >= arr[i]:
+            return "NO"
+    return "YES"
+
+assert monotonical([1, 7, 9]) == "YES"
+assert monotonical([1, 9, 7]) == "NO"
+assert monotonical([2, 2, 2]) == "NO"
+
+def main():
+    arr = list(map(float, input().split()))
+    print(monotonical(arr))
+
+if __name__ == "__main__":
+    main()
