a640fb90c7c4b1e607a15ae68b8181052161ffe7
Anuar <118943209+fyefh5@users.noreply.github.com>
2024-09-08 23:03:34 +0300

    hw1i: amend error and improved readability

--- hw1/hw1i.py
+++ hw1/hw1i.py
@@ -1,22 +1,17 @@
-def castle_if(a, b, c, d, e):
-    a, b, c = sorted([a, b, c])
-    d, e = sorted([d, e])
-    if d >= a and b >= e:
-        return "YES"
-    return  "NO"
+def castle_if(brick, hole):
+    brick = sorted(brick)
+    hole = sorted(hole)
+    return "YES" if brick[0] <= hole[0] and brick[1] <= hole[1] else "NO"
 
 
-assert castle_if(1, 1, 1, 1, 1) == "YES"
-assert castle_if(2, 2, 2, 1, 1) == "NO"
+assert castle_if([1, 1, 1], [1, 1]) == "YES"
+assert castle_if([2, 2, 2], [1, 1]) == "NO"
 
 
 def main():
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    d = int(input())
-    e = int(input())
-    ans = castle_if(a, b, c, d, e)
+    brick = [int(input()) for _ in range(3)]
+    hole = [int(input()) for _ in range(2)]
+    ans = castle_if(brick, hole)
     print(ans)
 
 
